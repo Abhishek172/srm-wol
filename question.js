dc4badbfa86405304a6b6bd459760bbc209cf50f
Abhishek172 <60508669+Abhishek172@users.noreply.github.com>
2020-02-11 09:22:21 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,36 @@
+var questions = [{
+    "question":"What does HTML stand for?",
+    "option1":"Hyperlinks and Text Markup Language",
+    "option2":"Hyper Text Markup Language",
+    "option3":"Home Tool Markup Language",
+    "option4":"None of the above",
+    "answer":"2"
+},{
+    "question":"What does CSS stand for?",
+    "option1":"Computer Style Sheets",
+    "option2":"Colorful Style Sheets",
+    "option3":"Cascading Style Sheets",
+    "option4":"None of the above",
+    "answer":"3"
+},{
+    "question":"Which CSS property sets a background image for an element?",
+    "option1":"background",
+    "option2":"background-attachment",
+    "option3":"background-image",
+    "option4":"None of the above",
+    "answer":"3"
+},{
+    "question":"Which are the objects used for storing data on the client provided by the HTML local storage?",
+    "option1":"window.localStorage",
+    "option2":"window.sessionStorage",
+    "option3":"both 1 and 2",
+    "option4":"None of the above",
+    "answer":"3"
+},{
+    "question":"What is the correct HTML tag for inserting a line break?",
+    "option1":"br /",
+    "option2":"br/",
+    "option3":"lb /",
+    "option4":"None of the above",
+    "answer":"1"
+}];
